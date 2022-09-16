63e023be0c866f516a12227c1e4d07914ca7b2c3
killerbee46 <sugamsingh10@gmail.com>
2022-09-16 10:14:32 +0545

    adding features WIP added registers

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -43,6 +43,7 @@ class RegisteredUserController extends Controller
             'name' => $request->name,
             'email' => $request->email,
             'password' => Hash::make($request->password),
+            'mobile'=>$request->mobile,
             'role'=>$request->role
         ]));
         event(new Registered($user));
